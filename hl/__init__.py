9f8132261dec10d9ec42d74fce5864bb118e1213
amineautomation2 <simobelmont4@gmail.com>
2026-04-13 18:58:32 +0100

    sdf

--- hl/__init__.py
+++ hl/__init__.py
@@ -16,7 +16,7 @@ from worker import (
 )
 
 
-def hl_runner(id_worker: int, max_workers: int):
+def hl_runner(id_worker: int, max_workers: int, sheet: str):
     pref = {
         # "profile.managed_default_content_settings.javascript": 2,
         # "profile.managed_default_content_settings.images": 2,
@@ -34,57 +34,45 @@ def hl_runner(id_worker: int, max_workers: int):
     #
     xlsx = get_xlsx_filepath("base.xlsx")
     # TODO add index to funds
-    # get_funds_url(driver, "Investment", xlsx)
-    # get_funds_url(driver, "ETF", xlsx)
-    # get_funds_url_mf(xlsx)
+    match sheet:
+        case "Investment":
+            get_funds_url(driver, "Investment", xlsx)
+            funds_it = get_xlsx_data(xlsx, "Investment")
+            it_config = {
+                "id_worker": id_worker,
+                "max_workers": max_workers,
+                "driver": driver,
+                "funds": funds_it[:5],
+                "sheet": "Investment",
+            }
+            process_worker_batch(it_config)
+            return
+        case "ETF":
+            get_funds_url(driver, "ETF", xlsx)
+            funds_etf = get_xlsx_data(xlsx, "ETF")
+            etf_config = {
+                "id_worker": id_worker,
+                "max_workers": max_workers,
+                "driver": driver,
+                "funds": funds_etf[:5],
+                "sheet": "ETF",
+            }
+            process_worker_batch(etf_config)
+            return
+        case "MF":
+            funds_mf = get_xlsx_data(xlsx, "MF")
+            mf_config = {
+                "id_worker": id_worker,
+                "max_workers": max_workers,
+                "driver": driver,
+                "funds": funds_mf[:5],
+                "sheet": "MF",
+            }
 
-    funds_etf = get_xlsx_data(xlsx, "ETF")
-    funds_it = get_xlsx_data(xlsx, "Investment")
-    funds_mf = get_xlsx_data(xlsx, "MF")
+            process_worker_batch(mf_config)
+            get_funds_url_mf(xlsx)
+            return
 
-    hl_funds = []
-    # hl_funds.extend(funds_etf)
-    # hl_funds.extend(funds_it)
-    # hl_funds.extend(funds_mf)
-
-    # print(f"len it {len(funds_it)}")
-    # print(f"len etf {len(funds_etf)}")
-    # print(f"len mf {len(funds_mf)}")
-    # print(f"len funds {len(hl_funds)}")
-
-    # Mf
-    # funds_per_worker = get_data_by_worker_id(id_worker, max_workers, funds_mf)
-    # out_csv = f"hl_mf_{id_worker}.csv"
-    # funds_mf = process_worker_batch(driver, out_csv, funds_per_worker)
-    # print(len(funds_per_worker))
-
-    it_config = {
-        "id_worker": id_worker,
-        "max_workers": max_workers,
-        "driver": driver,
-        "funds": funds_it[:5],
-        "sheet": "Investment",
-    }
-
-    etf_config = {
-        "id_worker": id_worker,
-        "max_workers": max_workers,
-        "driver": driver,
-        "funds": funds_etf[:5],
-        "sheet": "ETF",
-    }
-
-    mf_config = {
-        "id_worker": id_worker,
-        "max_workers": max_workers,
-        "driver": driver,
-        "funds": funds_mf[:5],
-        "sheet": "MF",
-    }
-
-    process_worker_batch(it_config)
-    process_worker_batch(etf_config)
-    process_worker_batch(mf_config)
     driver.quit()
 
 
@@ -104,5 +92,4 @@ def process_worker_batch(config: dict):
         funds_with_keywords = get_fund_keyword_mf(driver, funds_per_worker)
     fields = ["index", "name", "isin", "url", "keyword", "sheet"]
     write_csv_by_id(out_csv, funds_with_keywords, fields)
-    print(funds_with_keywords)
-    # save_xlsx(xlsx_path=xlsx, funds=funds_with_keywords, cols=cols, sheet="MF")
+    # print(funds_with_keywords)
--- main.py
+++ main.py
@@ -8,36 +8,23 @@ from worker import (
 )
 
 
-def list_of_strings(arg):
-    return arg.split(',')
-
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--id", type=str, help="id worker")
     parser.add_argument("--sheet", type=str, help="sheet name")
-    parser.add_argument("--sheets", type=list_of_strings, help="sheet name")
 
     args = parser.parse_args()
     out = "hl.xlsx"
     xlsx = os.path.join(os.getcwd(), "spreadsheet", out)
-    if args.id:
+    if args.id and args.sheet:
         # id_worker = int(sys.argv[1])
         start = time.perf_counter()
-        hl_runner(int(args.id), 5)
+        hl_runner(id_worker=int(args.id), max_workers=5, sheet=args.sheet)
         elapsed = time.perf_counter() - start
         print(f"Execution time: {elapsed:.2f} seconds.")
     elif args.sheet:
         merge_csv_to_xlsx(
             xlsx, ["name", "isin", "url", "keyword"], args.sheet)
-
-    elif args.sheets:
-        for s in args.sheets:
-            merge_csv_to_xlsx(
-                xlsx, ["name", "isin", "url", "keyword"], s)
-    else:
-        print("error: ", sys.argv)
-        exit(1)
 
 
 if __name__ == "__main__":
